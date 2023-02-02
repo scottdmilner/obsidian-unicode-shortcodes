eee5f4b5420f3c4ed281fa4516792cad825d2e9a
Scott Milner <scottdmilner@gmail.com>
2023-02-01 23:04:36 -0700

    add music symbols

---
+++ src/musicList.ts
@@ -0,0 +1,19 @@
+export const music = JSON.parse(`{
+    ":flat:": "♭",
+    ":sharp:": "♯",
+    ":natural:": "♮",
+    ":treble_clef:": "𝄞",
+    ":bass_clef:": "𝄢",
+    ":segno:": "𝄋",
+    ":coda:": "𝄌",
+    ":sixteenth_note:": "𝅘𝅥𝅯",
+    ":eighth_note:": "𝅘𝅥𝅮",
+    ":quarter_note:": "𝅘𝅥𝅯",
+    ":half_note:": "𝅗𝅥",
+    ":whole_note:": "𝅝",
+    ":sixteenth_rest:": "𝄿",
+    ":eighth_rest:": "𝄾",
+    ":quarter_rest:": "𝄽",
+    ":half_rest:": "𝄼",
+    ":whole_rest:": "𝄻"
+}`)
