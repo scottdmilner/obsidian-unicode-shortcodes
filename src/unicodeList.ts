bc7ef228eaddbe7f7864d7cebd45dc0650a70052
Scott Milner <scottdmilner@gmail.com>
2023-02-01 23:19:58 -0700

    add arrows

--- src/unicodeList.ts
+++ src/unicodeList.ts
@@ -2,5 +2,6 @@ import { emoji } from './emojiList';
 import { math } from './mathList';
 import { greek } from './greekList';
 import { music } from './musicList'
+import { arrow } from './arrowList';
 
-export const unicode = {...emoji, ...math, ...greek, ...music}
+export const unicode = {...emoji, ...math, ...greek, ...music, ...arrow}
